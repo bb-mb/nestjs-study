5f8fde946798ae4d4ac640991251084da60eb6df
soso01 <6125024@gmail.com>
2022-05-21 09:23:43 +0900

    <refactor> login page 분리

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,11 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import Link from "next/link";
 
-import { Login } from "@/components/login";
+import { Flex } from "@chakra-ui/react";
 
 import type { NextPage } from "next";
 
 const HomePage: NextPage = () => {
-  return <Login />;
+  return (
+    <Flex
+      bgColor='white'
+      direction='column'
+      borderRadius={12}
+      padding={20}
+      gap={20}
+    >
+      <Link href='/login'>login</Link>
+      <Link href='/sign-up'>sign-up</Link>
+    </Flex>
+  );
 };
 
 export default HomePage;
---
+++ client/src/pages/login.tsx
@@ -0,0 +1,9 @@
+import { Login } from "@/components/login";
+
+import type { NextPage } from "next";
+
+const LoginPage: NextPage = () => {
+  return <Login />;
+};
+
+export default LoginPage;
