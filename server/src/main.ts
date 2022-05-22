1435e66478eeaa92a8a7573243724586ed5bbc02
soso01 <6125024@gmail.com>
2022-05-22 16:42:03 +0900

    <feat> validationPipe 전역 설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DBService } from './db.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
   const dbService: DBService = app.get(DBService);
   dbService.enableShutdownHooks(app);
   await app.listen(3000);
