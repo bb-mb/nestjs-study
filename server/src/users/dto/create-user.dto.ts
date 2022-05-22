557a812d5c989fb372d77288716b1de246340a15
soso01 <6125024@gmail.com>
2022-05-22 16:23:57 +0900

    <feat> create-user dto 작성

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  password: string;
+}
